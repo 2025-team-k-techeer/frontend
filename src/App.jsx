0b2d9f28790a8fcba0c721948bb7bc1eb27a8722
Eunhea411 <ehjo0504@gmail.com>
2025-07-15 20:54:29 +0900

    add issue_template

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import LogIn from './pages/LogIn';
 // import ARScene from './pages/ARScene';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
         {/* <Route path="/ar" element={<ARScene />} /> */}
       </Routes>
     </BrowserRouter>
