b0ef1fd71b9ffe43664701a3b64882edf4a3dfad
minseon0201 <minseon721921@gmail.com>
2025-07-15 23:03:34 +0900

    chore: vercel-analytics 라이브러리 추가 #5

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 // import ARScene from './pages/ARScene';
 import AR from './pages/AR';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AR />} />
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/ar" element={<ARScene />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AR />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/ar" element={<ARScene />} /> */}
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </div>
   );
 }
