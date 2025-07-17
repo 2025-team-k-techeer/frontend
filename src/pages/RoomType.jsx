f74b1067a32ac72a9471756ba355db2ee7a6a7c7
Eunhea411 <ehjo0504@gmail.com>
2025-07-17 19:39:39 +0900

    Title과 ButtonAction 추가

--- src/pages/RoomType.jsx
+++ src/pages/RoomType.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import HeaderBack from '../components/HeaderBack';
 import ButtonAction from '../components/Button/ButtonAction';
 import ButtonS from '../components/Button/ButtonS';
+import Title from '../components/Title/Title';
 
 function RoomType() {
   return (
     <div>
+      <HeaderBack
+        title="적었잖아요"
+        bgColor="bg-sage-bg" // 배경색을 sage로 변경
+        showBorder={true} // 구분선 보임 (true는 기본값이므로 생략 가능)
+      />
+      <Title />
       <ButtonAction>다음</ButtonAction>
     </div>
   );
