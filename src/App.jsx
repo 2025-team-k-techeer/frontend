9beb2360b9fd8f2cf502af8085799b466efca581
minseon0201 <minseon721921@gmail.com>
2025-07-19 23:58:31 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ export default function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/StyleList" element={<StyleList />} />
+            {/* <Route path="/StyleList" element={<StyleList />} /> */}
           </Routes>
         </BrowserRouter>
       </AuthProvider>
