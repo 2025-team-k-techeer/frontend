caaf196941a55323d1cc868d92f895ad90f12356
Eunhea411 <ehjo0504@gmail.com>
2025-07-19 11:04:07 +0900

    충돌 수정

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,8 @@ import ProfilePage from './pages/ProfilePage.jsx';
 import ManualPage from './pages/ManualPage.jsx';
 import RoomType from './pages/RoomType.jsx';
 import UploadPage from './pages/UploadPage.jsx';
-<<<<<<< HEAD
-import StyleList from './pages/StyleList.jsx';
-export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/RoomType" element={<RoomType />} />
-          <Route path="/library" element={<LibraryPage />} />
-          <Route path="/RoomType" element={<RoomType />} />
-          <Route path="/ManualPage" element={<ManualPage />} />
-          <Route path="/home" element={<MainPage />} />
-          <Route path="/upload" element={<UploadPage />} />
-          <Route path="/library" element={<LibraryPage />} />
-          <Route path="/manual" element={<ManualPage />} />
-          <Route path="/Profilepage" element={<ProfilePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<StyleList />} />
-        </Routes>
-      </BrowserRouter>
-=======
 import LoginPage from './pages/LoginPage.jsx';
+import StyleList from './pages/StyleList.jsx';
 import { AuthProvider } from './contexts/AuthProvider.jsx'; //전역 상태로도 관리를 위함함
 
 export default function App() {
@@ -45,10 +25,10 @@ export default function App() {
             <Route path="/upload" element={<UploadPage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/StyleList" element={<StyleList />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
->>>>>>> d18ba11dca5f20e9fb851b9d1994b89b578037fc
       <Analytics />
     </div>
   );
