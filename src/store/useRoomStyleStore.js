9380736876d0bb4929f3a48060b8b1982ba0b390
minseon0201 <minseon721921@gmail.com>
2025-07-22 16:23:13 +0900

    스타일 전역저장 및 api 코드 작성 #53

--- src/store/useRoomStyleStore.js
+++ src/store/useRoomStyleStore.js
@@ -1,15 +1,16 @@
 import { create } from 'zustand';
 
 export const useRoomStyleStore = create((set) => ({
-  roomType: null,
-  setRoomType: (roomType) => set({ roomType }),
+  image_url: null,
+  setImageUrl: (image_url) => set({ image_url }),
+  room_type: null,
+  setRoomType: (room_type) => set({ room_type }),
   style: null,
   setStyle: (style) => set({ style }),
-  detail: '',
-  setDetail: (detail) => set({ detail }),
-  // 필요하다면 imageUrl, imageFilename 필드 추가
-  imageUrl: null,
+  prompt: '',
+  setPrompt: (prompt) => set({ prompt }),
+  // 필요하다면 imageFilename 필드 추가
   imageFilename: null,
   setImageInfo: ({ url, filename }) =>
-    set({ imageUrl: url, imageFilename: filename }),
+    set({ image_url: url, imageFilename: filename }),
 }));
