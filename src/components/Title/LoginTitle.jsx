1e818094d0cb999fbb671729744c541d8a3986e5
Eunhea411 <ehjo0504@gmail.com>
2025-07-22 22:41:58 +0900

    메인 타이틀

--- src/components/Title/LoginTitle.jsx
+++ src/components/Title/LoginTitle.jsx
@@ -1,8 +1,8 @@
 const LoginTitle = () => (
   //로그인 페이지 제목
   <div className="text-center mb-10">
-    <h1 className="text-5xl font-black text-brand-charcoal">집 꾸</h1>
-    <p className="text-gray-500 mt-2">
+    <h1 className="text-5xl font-PlexSans text-brand-charcoal">집 꾸</h1>
+    <p className="text-sage-accent mt-2">
       AI 인테리어의 시작, 지금 바로 함께하세요
     </p>
   </div>
