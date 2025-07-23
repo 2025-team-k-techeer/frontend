d00df17fd7afe5fb7afb5add4380bee89b728d31
Eunhea411 <ehjo0504@gmail.com>
2025-07-23 14:35:50 +0900

    더보기 수정

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -69,24 +69,18 @@ function DrawerMenu({ isOpen, onClose }) {
           <nav className="flex flex-col mx-auto">
             <a
               href="/upload"
-              className="text-2xl py-8 border-b border-gray-400 text-gray-700 w-full text-center"
+              className="text-2xl py-9 border-b border-gray-400 text-gray-700 w-full text-center"
             >
               시작하기
             </a>
-            <a href="/library" className="text-xl py-8 text-gray-500 mx-auto">
+            <a href="/library" className="text-xl py-9 text-gray-500 mx-auto">
               라이브러리
             </a>
-            <a href="/profile" className="text-xl py-8 text-gray-500 mx-auto">
+            <a href="/profile" className="text-xl py-9 text-gray-500 mx-auto">
               내 프로필
             </a>
-            <a href="/manual" className="text-xl py-8 text-gray-500 mx-auto">
+            <a href="/manual" className="text-xl py-9 text-gray-500 mx-auto">
               사용법
-            </a>
-            <a
-              href="/stylelist"
-              className="text-xl py-8  text-gray-500 mx-auto"
-            >
-              인테리어 스타일
             </a>
             {/* 로그아웃 버튼 */}
             <div
